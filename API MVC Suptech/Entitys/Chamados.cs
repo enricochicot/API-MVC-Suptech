e40f3fd8c3b9bb608eb384344f5b6333bb50eace
Enrico Leite <enrico.leite@pca.com.br>
2025-10-12 14:38:15 -0300

    Adiciona classe Chamados e atualiza string de conexão
    Foi adicionada uma nova classe chamada `Chamados` no namespace `API_MVC_Suptech.Entitys`, com várias propriedades obrigatórias e opcionais. Um comentário foi incluído sobre a chave primária não implementada.
    
    Além disso, a string de conexão `DefaultConnection` foi alterada de uma variável de ambiente para uma string de conexão explícita, contendo detalhes do servidor, catálogo inicial, ID de usuário e senha.

---
+++ API MVC Suptech/Entitys/Chamados.cs
@@ -0,0 +1,20 @@
+﻿namespace API_MVC_Suptech.Entitys
+{
+    public class Chamados
+    {
+        public Guid ChamadoId { get; set; }
+        public int UsuarioId { get; set; }
+        public int TecnicoId { get; set; }
+        public int CategoriaId { get; set; }
+        public required string Titulo { get; set; }
+        public required string Descricao { get; set; }
+        public required int DataAbertura { get; set; }
+        public required string Status { get; set; }
+        public required int Prioridade { get; set; }
+        public string? SolucaoSugeridaPelaIA { get; set; }
+        public required int DataFechamento { get; set; }
+        public string? SolucaoAplicada { get; set; }
+
+        //PKchamado não implementado
+    }
+}
